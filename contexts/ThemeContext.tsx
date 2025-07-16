09224d1cd46e07622e6b589d18c8f36bde5141bd
stephenbessey <sbessey53@gmail.com>
2025-07-16 14:03:33 -0600

    Update and rename ThemeContext.tsx to contexts/ThemeContext.tsx

--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -1,4 +1,3 @@
-// contexts/ThemeContext.tsx
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
